b114c15d4da4293d52b43a85a8257b4dc31dcd3e
adnankhayrou <adnanekhayrou3@gmail.com>
2022-12-23 21:31:50 +0100

    adding data.js file and button start to move to the next step to start quiz and adding button submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,24 @@
-// const steps = document.querySelectorAll('.step');
-
-// steps.forEach((step) => {
-//   step.addEventListener('click', () => {
-//     // Update active step
-//     steps.forEach(s => s.classList.remove('active'));
-//     step.classList.add('active');
-//   });
-// });
-
-
 const steps = document.querySelectorAll('.step');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
+const nextBtn = document.querySelector('#start-button');
+const cards = document.querySelector('#cards-quiz');
+// const start = document.getElementById('button-start');
+document.querySelector("#cards-quiz").style.display = 'none';
+    document.querySelector("#start-button").addEventListener('click', function(){
+    document.querySelector("#cards-quiz").style.display = 'block';
+    document.querySelector("#card-info").style.display = 'none';
+});
+
+
 
 let currentStep = 0;
-
-
     const updateStep = stepIndex => {
         // Update active step
         steps.forEach(s => s.classList.remove('active'));
         steps[stepIndex].classList.add('active');
         
         // Update buttons
-        prevBtn.disabled = stepIndex === 0;
         nextBtn.disabled = stepIndex === steps.length - 1;
-      };
-      
-      prevBtn.addEventListener('click', () => {
-        currentStep--;
-        updateStep(currentStep);
-      });
-      
+      };      
       nextBtn.addEventListener('click', () => {
         currentStep++;
         updateStep(currentStep);
@@ -39,4 +27,32 @@ let currentStep = 0;
       updateStep(currentStep);
 
 
+// cards.innerHTML = `
+// <h2 class="title1">Quiz Time</h2>
+// <p class="question">${questions[0].qst}</p>
+
+//             <div class="cards-ans">
+//                 <div class="card">
+//                     <p class="answer">${questions[0].choice1}</p>
+//                 </div>
+    
+//                 <div class="card">
+//                     <p class="answer">${questions[0].choice2}</p>
+//                 </div>
+    
+//                 <div class="card">
+//                     <p class="answer">${questions[0].choice3}</p>
+//                 </div>
+    
+//                 <div class="card">
+//                     <p class="answer">${questions[0].choice4}</p>
+//                 </div>
+
+//                 <div class="stepper-nav">
+//                 <!-- <button class="prev-btn">Previous</button> -->
+//                 <button type="submit" class="submit-btn">Submit</button>
+//             </div>
+
+                  
+//             </div> `
 
